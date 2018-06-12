22760c610a81ed951be67b96c4f34894644d50a6
Ed Shell <shelleg@g.cofc.edu>
2018-06-11 22:44:29 -0400

    files for Trivia Game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -42,26 +42,75 @@ function decrement() {
 }
 
 function stop() {
-    $(".container").text("Trivia is over!" + " Correct Answers= 1. c, 2. b, 3. a, 4. c, 5. b, 6. d");
+    $(".container").text("Trivia is over! " + "Correct: " + right + " Wrong: " + wrong);
 }
 
 function q1() {
     // $(".timer").html("<h2>" + number + "</h2>");
     run();
     $(".question").text("Chance the Rapper wears an iconic hat, what number is on the front?");
-    $(".answers").html("<p>a. 4</p>" + "<br>" + "<p>b. 10</p>" + "<br>" + "<p>c. 3</p>" + "</br>" + "<p>d. 1/3</p>");
+    $(".answers").text("a. 4");
+    $(".answers2").text("b. 3");
+    $(".answers3").text("c. 20");
+    $(".answers4").text("b. 1/3");
 
-    $(".answers").on("click", function() {
+    $(".answers2").on("click", function () {
+        right++;
+        console.log(right);
         q2();
-    })
+    });
+
+    $(".answers").on("click", function () {
+        wrong++;
+        console.log(wrong);
+        q2();
+    });
+
+    $(".answers3").on("click", function () {
+        wrong++;
+        console.log(wrong);
+        q2();
+    });
+    
+    $(".answers4").on("click", function () {
+        wrong++;
+        console.log(wrong);
+        q2();
+    });
 }
 
 function q2() {
     // $(".timer").text("You have 15 seconds for this question");
     $(".question").text("Which of the following rappers won a Nobel Prize in Literature?");
-    $(".answers").html("<p>a. Lil Pump</p>" + "<br>" + "<p>b. Kendrick Lamar</p>" + "<br>" + "<p>c. J. Cole</p>" + "</br>" + "<p>d. Coolio</p>");
+    $(".answers").text("a. Kendrick Lamar");
+    $(".answers2").text("b. Lil Pump");
+    $(".answers3").text("c. J. Cole");
+    $(".answers4").text("b. Chance the Rapper");
 
-    $(".answers").on("click", function() {
+    $(".answers").on("click", function () {
+
+        right++;
+        console.log(right);
+        q3();
+    })
+
+    $(".answers2").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q3();
+    })
+
+    $(".answers4").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q3();
+    })
+    $(".answers3").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
         q3();
     })
 }
@@ -69,41 +118,149 @@ function q2() {
 function q3() {
     // $(".timer").text("You have 15 seconds for this question");
     $(".question").text("'Scalamoosh, Scalamoosh, will you do the fandango' is a famous lyric from which song?");
-    $(".answers").html("<p>a. Bohemian Rhapsody</p>" + "<br>" + "<p>b. God's Plan</p>" + "<br>" + "<p>c. Romanian Phapody</p>" + "</br>" + "<p>d. Stairway to Heaven</p>");
+    $(".answers").text("a. Romanian Phapody");
+    $(".answers2").text("b. God's Plan");
+    $(".answers3").text("c. Bohemian Rhapsody");
+    $(".answers4").text("b. Don't Stop Believin'");
 
-    $(".answers").on("click", function() {
+    $(".answers3").on("click", function () {
+
+        right++;
+        console.log(right);
         q4();
-    })
+    });
+
+    $(".answers").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q4();
+    });
+
+    $(".answers2").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q4();
+    });
+    $(".answers4").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q4();
+    });
 }
 
 function q4() {
     // $(".timer").text("You have 15 seconds for this question");
     $(".question").text("Who loves it when you call him big Poppa?");
-    $(".answers").html("<p>a. Pac</p>" + "<br>" + "<p>b. Kenny Chesneyr</p>" + "<br>" + "<p>c. Notorious B.I.G.</p>" + "</br>" + "<p>d. Mason Ramsey</p>");
+    $(".answers").text("a. Pac");
+    $(".answers2").text("b. Kenny Chesney");
+    $(".answers3").text("c. Mason Ramsey");
+    $(".answers4").text("b. Notorious B.I.G.");
 
-    $(".answers").on("click", function() {
+    $(".answers4").on("click", function () {
+
+        right++;
+        console.log(right);
         q5();
-    })
+    });
+
+    $(".answers").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q5();
+    });
+
+    $(".answers3").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q5();
+    });
+
+    $(".answers2").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q5();
+    });
 }
 
 function q5() {
     // $(".timer").text("You have 15 seconds for this question");
     $(".question").text("What famous actor is also the lead singer for 30 Seconds to Mars?");
-    $(".answers").html("<p>a. Millie Bobbie Brown</p>" + "<br>" + "<p>b. Jared Leto</p>" + "<br>" + "<p>c. Helen Meren</p>" + "</br>" + "<p>d. Seth Rogan</p>");
+    $(".answers").text("a. Helen Meren");
+    $(".answers2").text("b. Jared Leto");
+    $(".answers3").text("c. Millie Bobbie Brown");
+    $(".answers4").text("b. Seth Rogan");
 
-    $(".answers").on("click", function() {
+    $(".answers2").on("click", function () {
+
+        right++;
+        console.log(right);
         q6();
-    })
+    });
+
+    $(".answers").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q6();
+    });
+
+    $(".answers3").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q6();
+    });
+
+    $(".answers4").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        q6();
+    });
 }
 
 function q6() {
     // $(".timer").text("You have 15 seconds for this question");
     $(".question").text("Imma let you finish, but who had the best music video of all time?");
-    $(".answers").html("<p>a. T-Swift</p>" + "<br>" + "<p>b. Rebecca Black</p>" + "<br>" + "<p>c. Ye</p>" + "</br>" + "<p>d. Bey</p>");
+    $(".answers").text("a. Ye");
+    $(".answers2").text("b. T-Swift");
+    $(".answers3").text("c. Bey");
+    $(".answers4").text("b. Rebecca Black");
 
-    $(".answers").on("click", function() {
+    $(".answers3").on("click", function () {
+
+        right++;
+        console.log(right);
         stop();
-    })
+    });
+
+    $(".answers").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        stop();
+    });
+
+    $(".answers2").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        stop();
+    });
+
+    $(".answers4").on("click", function () {
+
+        wrong++;
+        console.log(wrong);
+        stop();
+    });
+
 }
 
 // function stop() {
